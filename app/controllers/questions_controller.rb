3e92d223f2412ad4e67ee544b2aba9fa0a94d4e2
dn.artego <dn.artego@gmail.com>
2023-01-28 21:06:31 +0300

    lesson_5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,29 @@
 class QuestionsController < ApplicationController
+  before_action :set_question!, only: %i[show destroy edit update]
+
+  def show
+    @answer = @question.answers.build
+    @answers = Answer.where(question_id: @question[:id]).reverse
+  end
+
+  def destroy
+    @question.destroy
+    flash[:success] = "Question deleted!"
+    redirect_to questions_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update question_params
+      flash[:success] = "Question updated!"
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
   def index
     @questions = Question.all
   end
@@ -10,36 +35,11 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new question_params
     if @question.save
-      flash[:success] = 'Question created!'
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :new
     end
-  end
-
-  def edit
-    @question = Question.find_by id: params[:id]
-  end
-
-  def update
-    @question = Question.find_by id: params[:id]
-    if @question.update question_params
-      flash[:success] = 'Question updated!'
-      redirect_to questions_path
-    else
-      render :edit
-    end
-  end
-
-  def show
-    @question = Question.find_by id: params[:id]
-  end
-
-  def destroy
-    @question = Question.find_by id: params[:id]
-    @question.destroy
-    flash[:success] = 'Question deleted!'
-    redirect_to questions_path
   end
 
   private
@@ -48,4 +48,7 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body)
   end
 
+  def set_question!
+    @question = Question.find params[:id]
+  end
 end
