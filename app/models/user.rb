148502fcee668188b1c686732436ccd045d60766
dn.artego <dn.artego@gmail.com>
2023-02-01 23:49:55 +0300

    cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,39 @@
 class User < ApplicationRecord
-  attr_accessor :old_password
+  attr_accessor :old_password, :remember_token
 
   has_secure_password validations: false
 
   validate :password_presence
   validate :correct_old_password, on: :update, if: -> { password.present? }
   validates :password, confirmation: true, allow_blank: true,
-                        length: { minimum: 8, maimum: 70 }
+                       length: { minimum: 8, maimum: 70 }
 
   validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
   validate :password_complexity
 
+  def remember_me
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_column :remember_token_digest, digest(remember_token)
+  end
+
+  def forget_me
+    update_column :remember_token_digest, nil
+    self.remember_token = nil
+  end
+
+  def remember_token_authenticated?(remember_token)
+    return false unless remember_token_digest.present?
+
+    BCrypt::Password.new(remember_token_digest).is_password?(remember_token)
+  end
+
   private
+
+  def digest(string)
+    cost = ActiveModel::SecurePassword.
+      min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
   def correct_old_password
     return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
